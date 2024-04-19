2aa8baa668423f276fe4cccebe465819f7dfd2cb
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-04-19 13:28:06 +0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,3 @@
+getgenv().Key = "MARU-VD3CS-1H3V7-WVH1-E0988-CXG2"
+getgenv().id = "1092091088422043648"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
